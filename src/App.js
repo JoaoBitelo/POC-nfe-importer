e3d2e648d1acfb738c3cdb0249cbb6d358463f10
Joao Bitelo <joao.octavio@gmail.com>
2021-07-10 00:01:12 -0300

    adiciona drag

--- src/App.js
+++ src/App.js
@@ -1,47 +1,51 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useCallback } from "react";
+import { useDropzone } from "react-dropzone";
 
-function sender() {
-    let url = "http://127.0.0.1:3000/import";
-    return fetch(url, {
-        method: "POST",
-        headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            login: "a",
-            token: "b",
-        }),
-    })
-        .then((response) => {
-            console.log("response", response);
-        })
-        .catch((error) => {
-            console.error("erro", error);
+export default function MyDropzone() {
+    const onDrop = useCallback((acceptedFiles) => {
+        acceptedFiles.forEach((file) => {
+            const reader = new FileReader();
+
+            reader.onabort = () => console.log("file reading was aborted");
+            reader.onerror = () => console.log("file reading has failed");
+            reader.onload = () => {
+                // Do whatever you want with the file contents
+                const binaryStr = reader.result;
+                console.log("binaryStr", binaryStr);
+                //console.log("aa", aa);
+                let url = "http://127.0.0.1:3000/import";
+                fetch(url, {
+                    method: "POST",
+                    headers: {
+                        Accept: "application/json",
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        data: binaryStr,
+                    }),
+                })
+                    .then((response) => {
+                        console.log("response", response);
+                    })
+                    .catch((error) => {
+                        console.error("erro", error);
+                    });
+            };
+            reader.readAsText(file);
         });
-}
+    }, []);
+    const { getRootProps, getInputProps, isDragActive } = useDropzone({
+        onDrop,
+    });
 
-function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <button onClick={sender}>Salvar</button>
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+        <div {...getRootProps()}>
+            <input {...getInputProps()} />
+            {isDragActive ? (
+                <p>Drop the files here ...</p>
+            ) : (
+                <p>Drag 'n' drop some files here, or click to select files</p>
+            )}
         </div>
     );
 }
-
-export default App;
